0a87de4ed697c947366ebc28b98021f433c7eb66
Alex Pham <925workk@gmail.com>
2020-05-19 22:30:27 -0600

    create context.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,5 +36,10 @@ export default class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
-
+    background: var(--mainBlue);
+    .nav-link{
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
 `
